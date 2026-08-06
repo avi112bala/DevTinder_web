b83b25e290365892945027d47c6bd2270dc09706
Avi112bala <avinash20802bala@gmail.com>
2026-08-06 06:45:47 +0530

    change base url

--- src/utils/Baseurl.ts
+++ src/utils/Baseurl.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "/api";
-export const BASE_URL = "http://16.192.150.153/api";
+export const BASE_URL = "/api";
+// export const BASE_URL = "http://16.192.150.153/api";
